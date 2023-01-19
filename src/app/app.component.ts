b8e3eee066985dcc3f07be681f98d0fdf102368a
Miguel Henriquez <miguelisaiashenriquez>
2023-01-19 17:04:47 -0600

    add game result logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,13 @@ export class AppComponent {
   title = 'rock-paper-scissors';
   showRules: boolean = false;
   score = 0;
+  readonly PAPER = 'paper';
+  readonly SCISSORS = 'scissors';
+  readonly ROCK = 'rock';
   userChoice = '';
   houseChoice = '';
-  choices = ['rock', 'paper', 'scissors'];
+  choices = [this.ROCK, this.PAPER, this.SCISSORS];
+  resultMessage = '';
 
   toggleRulesModal() {
     this.showRules = !this.showRules;
@@ -18,8 +22,40 @@ export class AppComponent {
 
   setUserChoice(choice: string) {
     this.userChoice = choice;
-    setTimeout(() => {
-      this.houseChoice = this.choices[Math.floor(Math.random() * 3)];
-    }, Math.ceil(Math.random() * 800) + 100);
+    // setTimeout(() => {
+    this.houseChoice = this.choices[Math.floor(Math.random() * 3)];
+    this.resultMessage = this.gameResult(this.userChoice, this.houseChoice);
+    // }, Math.ceil(Math.random() * 800) + 100);
+  }
+
+  reset() {
+    this.userChoice = '';
+    this.houseChoice = '';
+    this.resultMessage = '';
+  }
+
+  gameResult(userChoice: string, houseChoice: string) {
+    const userWins = this.winsAgainst(userChoice) === houseChoice;
+    const computerWins = this.winsAgainst(houseChoice) === userChoice;
+    let message = "it's a tie";
+    if (userWins) {
+      message = 'you win';
+      ++this.score;
+    } else if (computerWins) {
+      message = 'you lose';
+      --this.score;
+    }
+    return message;
+  }
+
+  winsAgainst(choice: string) {
+    switch (choice) {
+      case this.PAPER:
+        return this.ROCK;
+      case this.ROCK:
+        return this.SCISSORS;
+      default:
+        return this.PAPER;
+    }
   }
 }
