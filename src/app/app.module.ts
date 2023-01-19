ab239d264e905c292d4d5bdac758f6e0e4997f21
Miguel Henriquez <miguelisaiashenriquez>
2023-01-19 08:28:04 -0600

    add rules modal component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RulesModalComponent } from './components/rules-modal/rules-modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RulesModalComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/rules-modal/rules-modal.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rules-modal',
+  templateUrl: './rules-modal.component.html',
+})
+export class RulesModalComponent {}
